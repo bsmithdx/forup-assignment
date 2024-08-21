dfc78004c1678cc574a0b96ed0a18e7ffa712742
brendansmith <brendan.smith0325@gmail.com>
2024-08-21 10:46:33 -0400

    Install React

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+import App from './components/App'
+
+ReactDom.createRoot(document.getElementById('app')).render(
+    <App/>
+);
